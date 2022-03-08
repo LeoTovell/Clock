55807446e0a80959ea6407b6f44c3b33f62fb470
LeoTovell <51256511+LeoTovell@users.noreply.github.com>
2022-03-08 13:31:35 +0000

    Add files via upload

--- core/src/main/java/com/gdx/clock/clock.java
+++ core/src/main/java/com/gdx/clock/clock.java
@@ -36,20 +36,49 @@ public class clock extends ApplicationAdapter {
 		LocalTime localTime = LocalTime.now();
 		int seconds = Integer.parseInt(secondsFormat.format(localTime));
 		float percentSeconds = seconds/60f;
-		System.out.println(percentSeconds);
 		float secondsAngle = percentSeconds * 360;
-		secondsAngle = (float) (secondsAngle * Math.PI / 180);
+		secondsAngle = (float) ((secondsAngle * Math.PI / 180) - 90 * Math.PI / 180);
 		
 		
-		float x = (float) (clockCenterX + 180 * Math.cos(secondsAngle));
-		float y = (float) (clockCenterY + 180 * Math.sin(secondsAngle));
+		float secondX = (float) (clockCenterX + 180 * Math.cos(-secondsAngle));
+		float secondY = (float) (clockCenterY + 180 * Math.sin(-secondsAngle));
+		
+		int minutes = Integer.parseInt(minutesFormat.format(localTime));
+		float percentMinutes = minutes/60f;
+		float minutesAngle = percentMinutes * 360;
+		minutesAngle = (float)((minutesAngle * Math.PI / 180) - 90 * Math.PI / 180);
+		
+		float minuteX = (float) (clockCenterX + 160 * Math.cos(-minutesAngle));
+		float minuteY = (float) (clockCenterY + 160 * Math.sin(-minutesAngle));
+		
+		int hours = Integer.parseInt(hoursFormat.format(localTime));
+		float percentHours = hours/60f;
+		float hoursAngle = percentHours * 360;
+		hoursAngle = (float)((hoursAngle * Math.PI / 180) - 90 * Math.PI / 180);
+		
+		float hourX = (float) (clockCenterX + 120 * Math.cos(-hoursAngle));
+		float hourY = (float) (clockCenterY + 120 * Math.sin(-hoursAngle));
 		
 		ScreenUtils.clear(Color.WHITE);
 		
 		sr.begin(ShapeType.Line);
 		sr.setColor(Color.BLACK);
 		sr.circle(clockCenterX, clockCenterY, 200);
-		sr.rectLine(clockCenterX, clockCenterY, x, y, 20);
+		sr.end();
+		
+		sr.begin(ShapeType.Filled);
+		sr.setColor(Color.BLACK);
+		sr.rectLine(clockCenterX, clockCenterY, hourX, hourY, 7);
+		sr.end();
+		
+		sr.begin(ShapeType.Filled);
+		sr.setColor(Color.BLACK);
+		sr.rectLine(clockCenterX, clockCenterY, minuteX, minuteY, 7);
+		sr.end();
+		
+		sr.begin(ShapeType.Filled);
+		sr.setColor(Color.RED);
+		sr.rectLine(clockCenterX, clockCenterY, secondX, secondY, 5);
 		sr.end();
 		
 	}
